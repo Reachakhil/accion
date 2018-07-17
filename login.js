a02d7c1ef2750f26fb3f839e96e59167607a1e56
bhardwaj.akhil96@gmail.com <akhil.bhardwaj@accionlabs.com>
2018-07-17 16:23:14 +0530

    loginproject

---
+++ login.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+	var button = document.getElementById("btn");
+	var input1 = document.getElementById("name1");
+	var input2 = document.getElementById("password");
+
+	button.onclick = function() {
+	
+	if(input1.value=="")
+	alert("name not given");
+	else if(input2.value== "")
+	alert("password not given");
+	else
+	console.log('clicked' , input1.value , input2.value);
+	alert("login succeful");
+	
+
+}
+}
