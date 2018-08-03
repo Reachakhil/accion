27a2ac759aa324f16b85ce250649816db3ee730d
bhardwaj.akhil96@gmail.com <akhil.bhardwaj@accionlabs.com>
2018-08-03 15:19:33 +0530

    new

--- assignments/done/booleancalc/booleancalc.js
+++ assignments/done/booleancalc/booleancalc.js
@@ -1,20 +1,23 @@
-window.onload=function(){
+function result1() {
 var input1=document.getElementById("id1");
 var input2=document.getElementById("id2");
 var res='';
 
 var button = document.getElementById("submit");
 var btn2 =  document.getElementById("btn2");
-button.onclick = function(){
 
+
+
+if(button.value== 'on'){
  res= (input1.value|input2.value);
-	alert(res);
+ console.log(button.value);
+
 }
 
-btn2.onclick=function(){
+else if(btn2.value== 'on'){
 	var res= (input1.value&input2.value);
-	alert( res);
+	console.log(button.value);
+	
 }
-document.getElementById("result").innerHTML=res;
-
+document.getElementById("result").innerHTML="The result is"+res;
 }
--- assignments/done/dayofweek/Day-of-week.js
+++ assignments/done/dayofweek/Day-of-week.js
@@ -2,5 +2,5 @@
 function day(){
 			var date = new Date(document.getElementById("date").value);
     			var days = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-      			document.write(days[date.getDay()]);
+      			document.getElementById("res").innerHTML=days[date.getDay()];
 			}
